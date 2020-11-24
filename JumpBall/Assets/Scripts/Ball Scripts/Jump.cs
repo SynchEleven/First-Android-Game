76b3c493247d40091afe3d0786f9482a5d9dffe9
SynchEleven <syncheleven@gmail.com>
2020-11-24 12:47:02 +0100

    Ball Sprite + Jump Physics
    Added the Ball Sprite png
    Added the c# script for on-touch jumping

---
+++ JumpBall/Assets/Scripts/Ball Scripts/Jump.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    private float thrust = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Touch touch in Input.touches)
+        {
+            if(touch.phase == TouchPhase.Began)
+            {
+                rb2D.AddForce(transform.up * thrust, ForceMode2D.Impulse);
+            }
+        }
+    }
+}
