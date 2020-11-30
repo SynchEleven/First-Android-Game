791190296ad9b92c87de2bfdc6b3c42fd06d275f
Luktronic <luka.furundzija@gmx.at>
2020-11-30 21:45:21 +0100

    Reworked basic jump movement
    -Fixed double jumping in air

--- JumpBall/Assets/Scripts/Ball Scripts/Movement.cs
+++ JumpBall/Assets/Scripts/Ball Scripts/Movement.cs
@@ -7,25 +7,44 @@ public class Movement : MonoBehaviour
     private Rigidbody2D rb2D;
     private CircleCollider2D cc2D;
     public float thrust = 5.0f;
+    public int jumpLag = 10;
+    private int lagLeft;
     private Vector2 startPosition;
     private Vector2 endPosition;
+    private bool canJump;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2D = gameObject.GetComponent<Rigidbody2D>();
         cc2D = gameObject.GetComponent<CircleCollider2D>();
+        lagLeft = jumpLag;
+        canJump = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        foreach (Touch touch in Input.touches)
+
+        Debug.Log(System.Diagnostics.Process.GetCurrentProcess());
+
+        if (lagLeft > 0)
+            lagLeft--;
+
+
+            foreach (Touch touch in Input.touches)
         {
             if (touch.phase == TouchPhase.Began)
             {
+                if (cc2D.IsTouchingLayers(LayerMask.GetMask("GroundCollisions")) && lagLeft == 0)
+                    canJump = true;
                 startPosition = touch.position;
                 //rb2D.AddForce(transform.up * thrust, ForceMode2D.Impulse);
+            }
+            else if(touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
+            {
+                if (cc2D.IsTouchingLayers(LayerMask.GetMask("GroundCollisions")) && lagLeft == 0)
+                    canJump = true;
             }
             else if (touch.phase == TouchPhase.Ended)
             {
@@ -33,10 +52,17 @@ public class Movement : MonoBehaviour
 
                 Vector2 movementVector = startPosition - endPosition;
 
-                if (cc2D.IsTouchingLayers(LayerMask.GetMask("GroundCollisions")))
+                if (movementVector.y > 250)
+                    movementVector.y = 250;
+
+                if (canJump && lagLeft == 0)
                 {
                     rb2D.AddForce(movementVector * 0.07f, ForceMode2D.Impulse);
+                    canJump = false;
+                    lagLeft = jumpLag;
+                    Debug.Log("kekw");
                 }
+
             }
         }
 
